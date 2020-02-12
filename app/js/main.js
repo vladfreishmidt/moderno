90780b92bb327743371212e79e62b8c14cea7842
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-02-12 10:19:13 +0200

    Added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 $(function () {
+
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: '12px',
+        readOnly: true
+    });
+
+    $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: false
+    });
+
     var mixer = mixitup('.products__inner-box');
 
 
